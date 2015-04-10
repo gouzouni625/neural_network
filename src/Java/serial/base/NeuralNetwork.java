ac62c2dc16e42d396bc285720202e8f16e84095a
Georgios Ouzounis <geoconstructor625@gmail.com>
2015-04-10 18:46:45 +0300

    Added comments to NeuralNetwork class. Replaced train package with trainers.

--- src/Java/serial/base/NeuralNetwork.java
+++ src/Java/serial/base/NeuralNetwork.java
@@ -4,10 +4,22 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.lang.Math;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+/** \class NeuralNetwork.
+ *  \brief A class implementing a neural network. Includes the network's
+ *         parameters, training and evaluating methods.
+ */
 public class NeuralNetwork{
 
+  /** \brief The constructor of the class. Randomly initializes the
+   *         network's parameters with values in [-0.25, 0.25).
+   * 
+   *  \param sizesOfLayers The number of neurons in each layer including
+   *         the input and output layers. The number of layers is equal
+   *         to sizesOfLayers.length.
+   */
   public NeuralNetwork(int[] sizesOfLayers){
     sizesOfLayers_ = sizesOfLayers;
     numberOfLayers_ = sizesOfLayers_.length;
@@ -30,6 +42,13 @@ public class NeuralNetwork{
     }
   }
 
+  /** \brief Applies an input to the neural network and returns its output.
+   * 
+   *  \param input The input to the neural network. The length of the input
+   *         must be equal to sizesOfLayers_[0].
+   *  \return The output of the layer for the given input. The length of the
+   *          output is equal to sizesOfLayers_[sizesOfLayers_.length - 1].
+   */
   public double[] feedForward(double[] input){
     double[] inputBuffer = input.clone();
 
@@ -52,14 +71,25 @@ public class NeuralNetwork{
     return output;
   }
 
+  /** \brief Trains the neural network on a given set of data.
+   * 
+   *  \param trainingSet The set of data on which the neural network will
+   *         be trained. The length of each sample of the data set must be
+   *         equal to sizesOfLayers_[0].
+   *  \param labels The labels for the training set. The length of each
+   *         sample of the labels set must be equal to
+   *         sizesOfLayers_[sizesOfLayers_.length - 1].
+   *  \param trainingSetSize The number of training samples in the training set.
+   *  \param numberOfIterations The number of iterations of the training method. 
+   *  \param gamma Parameter gamma of the training method(gradient descent).
+   */
   public void train(double[][] trainingSet, double[][] labels,
                     int trainingSetSize, int numberOfIterations,
                     double gamma){
-    int sampleSize = sizesOfLayers_[0];
-    int labelSize = sizesOfLayers_[numberOfLayers_ - 1];
-
+    
     double[][][] nablaTheta = new double[numberOfLayers_ - 1][][];
     for(int i = 0;i < numberOfLayers_ - 1;i++){
+      
       nablaTheta[i] = new double[sizesOfLayers_[i + 1]][];
       for(int j = 0;j < sizesOfLayers_[i + 1];j++){
         nablaTheta[i][j] = new double[sizesOfLayers_[i] + 1];
@@ -70,26 +100,27 @@ public class NeuralNetwork{
       }
     }
 
-    // Training the network.
+    // Training loop.
     for(int iteration = 0;iteration < numberOfIterations;iteration++){
 
-      // Calculating the derivative of the cost function for each sample.
+      // Calculating the derivative of the cost function for each sample
+      // using the back propagation algorithm.
       for(int sample = 0;sample < trainingSetSize;sample++){
         this.backPropagation(trainingSet[sample], labels[sample], nablaTheta);
       }
 
-      // Updating network's parameters.
+      // Updating network's parameters using the gradient descent algorithm.
       for(int i = 0;i < numberOfLayers_ - 1;i++){
-
         for(int j = 0;j < sizesOfLayers_[i + 1];j++){
-
           biases_[i][j] -= gamma * nablaTheta[i][j][0] / trainingSetSize;
           for(int k = 0;k < sizesOfLayers_[i];k++){
-            weights_[i][j][k] -= gamma * nablaTheta[i][j][k + 1] / trainingSetSize;
+            weights_[i][j][k] -= gamma * nablaTheta[i][j][k + 1] /
+                                 trainingSetSize;
           }
         }
       }
 
+      // Set nablaTheta to zero.
       for(int i = 0;i < numberOfLayers_ - 1;i++){
         for(int j = 0;j < sizesOfLayers_[i + 1];j++){
           for(int k = 0;k < sizesOfLayers_[i] + 1;k++){
@@ -101,66 +132,76 @@ public class NeuralNetwork{
     }
   }
 
+  /** \brief getter function for the number of this neural network's layers.
+   * 
+   *  \return The number of this neural network's layers.
+   */
   public int getNumberOfLayers(){
     return numberOfLayers_;
   }
 
-  public void saveNetwork(String fileName){
-    FileOutputStream fileOutputStream = null;
-    DataOutputStream dataOutputStream = null;
+  /** \brief Saves the parameters of the neural network to a binary file.
+   * 
+   *  \param path The absolute, or relative path of the file where the
+   *         parameters will be saved.
+   *         
+   *  \throws FileNotFoundException
+   *  \throws IOException
+   */
+  public void saveNetwork(String path) throws FileNotFoundException,
+                                              IOException{
     
-    try{
-      fileOutputStream = new FileOutputStream(fileName);
-      dataOutputStream = new DataOutputStream(fileOutputStream);
+    FileOutputStream fileOutputStream = new FileOutputStream(path);
+    DataOutputStream dataOutputStream = new DataOutputStream(fileOutputStream);
   
-      for(int i = 0;i < numberOfLayers_ - 1;i++){
-        for(int j = 0;j < sizesOfLayers_[i + 1];j++){
-          dataOutputStream.writeDouble(biases_[i][j]);
+    for(int i = 0;i < numberOfLayers_ - 1;i++){
+      for(int j = 0;j < sizesOfLayers_[i + 1];j++){
+        dataOutputStream.writeDouble(biases_[i][j]);
           
-          for(int k = 0;k < sizesOfLayers_[i];k++){
-            dataOutputStream.writeDouble(weights_[i][j][k]);
-          }
+        for(int k = 0;k < sizesOfLayers_[i];k++){
+          dataOutputStream.writeDouble(weights_[i][j][k]);
         }
       }
-      
-      System.out.print("The network was successfully saved in file");
-      System.out.println(" " + fileName);
     }
-    catch(Exception e){
-      System.out.println("Error while trying to save the network.");
-      System.out.println("The network was not saved.");
-    }
+    dataOutputStream.close();        
   }
 
-  public void loadNetwork(String fileName){
-    FileInputStream fileInputStream = null;
-    DataInputStream dataInputStream = null;
+  /** \brief Load the parameters of a neural network to this neural network.
+   * 
+   *  \param path The path of the file where the parameters are saved. 
+   *  
+   *  \throws FileNotFoundException
+   *  \throws IOException
+   */
+  public void loadNetwork(String path) throws FileNotFoundException,
+                                              IOException{
     
-    try{
-      fileInputStream = new FileInputStream(fileName);
-      dataInputStream = new DataInputStream(fileInputStream);
+    FileInputStream fileInputStream = new FileInputStream(path);
+    DataInputStream dataInputStream = new DataInputStream(fileInputStream);
       
-      for(int i = 0;i < numberOfLayers_ - 1;i++){
-        for(int j = 0;j < sizesOfLayers_[i + 1];j++){
-          biases_[i][j] = dataInputStream.readDouble();
+    for(int i = 0;i < numberOfLayers_ - 1;i++){
+      for(int j = 0;j < sizesOfLayers_[i + 1];j++){
+        biases_[i][j] = dataInputStream.readDouble();
           
-          for(int k = 0;k < sizesOfLayers_[i];k++){
-            weights_[i][j][k] = dataInputStream.readDouble();
-          }
+        for(int k = 0;k < sizesOfLayers_[i];k++){
+          weights_[i][j][k] = dataInputStream.readDouble();
         }
       }
-      
-      System.out.print("A network was successfully loaded from file");
-      System.out.println(" " + fileName);
     }
-    catch(Exception e){
-      System.out.println("Error while trying to load a network.");
-    }
-
+    dataInputStream.close();
   }
 
+  /** \brief Implement the back propagation algorithm to calculate the
+   *         derivative of the cost function with respect to each parameter.
+   * 
+   *  \param sample The input to the neural network.
+   *  \param label The label of the input.
+   *  \param nablaTheta The array in which the derivative of the cost function
+   *         will be saved.
+   */
   private void backPropagation(double[] sample, double[] label,
                                double[][][] nablaTheta){
+    
     // Calculating the output of each neuron.
     double[] sampleBuffer = sample.clone();
     double[][] activations = new double[numberOfLayers_ - 1][];
@@ -204,7 +245,8 @@ public class NeuralNetwork{
       }
     }
 
-    // Calculating the derivative of the const function with respect to every parameter.
+    // Calculating the derivative of the cost function with respect to every
+    // parameter.
     for(int j = 0;j < sizesOfLayers_[1];j++){
       nablaTheta[0][j][0] += delta[0][j]; // biases.
       for(int k = 1;k < sizesOfLayers_[0] + 1;k++){
@@ -222,13 +264,18 @@ public class NeuralNetwork{
 
   }
 
+  /** \brief Activation function for the neurons.
+   * 
+   *  \param z The independent variable.
+   *  \return The sigmoid of z.
+   */
   private double sigmoid(double z){
     return (1 / (1 + Math.exp(-z)));
   }
 
-  private int numberOfLayers_;
-  private int[] sizesOfLayers_;
+  private int numberOfLayers_; //!< The number of layers of the neural network.
+  private int[] sizesOfLayers_; //!< The number of neurons in each layer.
 
-  private double[][][] weights_;
-  private double[][] biases_;
+  private double[][][] weights_; //!< dimensions are: layer, neuron, weight.
+  private double[][] biases_; //!< dimensions are: layer, neuron.
 };
---
+++ src/Java/serial/trainers/MNISTTrainer.java
@@ -0,0 +1,140 @@
+package trainers;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import java.util.Arrays;
+
+/** \class MNISTTrainer class. Trains a neural network on the MNIST database
+ *         of handwritten equation. The database can be found at
+ *         http://yann.lecun.com/exdb/mnist/  
+ */
+public class MNISTTrainer extends Trainer{
+  
+  public MNISTTrainer(int[] sizesOfLayers){
+    super(sizesOfLayers);
+  }
+  
+  public void load() throws FileNotFoundException, IOException{
+    numberOfTrainingSamples_ = 60000;
+    numberOfTestingSamples_ = 10000;
+    numberOfEpochs_ = 10;
+    batchSize_ = 1000;
+    gamma_ = 0.25;
+    
+    int sampleLength = sizesOfLayers_[0];
+    int numberOfLabels = sizesOfLayers_[sizesOfLayers_.length - 1];
+    
+    // Load the training set. =================================================
+    FileInputStream fileInputStream = new FileInputStream(
+                      "data/training_data/MNIST_data/train-images.idx3-ubyte");
+    fileInputStream.skip(16);
+    
+    trainingSet_ = new double[numberOfTrainingSamples_][sampleLength];
+    byte[] sampleBuffer = new byte[sampleLength];
+    
+    for(int i = 0;i < numberOfTrainingSamples_;i++){
+      fileInputStream.read(sampleBuffer);
+      
+      for(int j = 0;j < sampleLength;j++){
+        trainingSet_[i][j] = ((double)(sampleBuffer[j] & 0xFF)) / 127.5 - 1;
+      }
+    }
+    fileInputStream.close();
+    
+    // Load the training labels. ==============================================
+    fileInputStream = new FileInputStream(
+                      "data/training_data/MNIST_data/train-labels.idx1-ubyte");
+    fileInputStream.skip(8);
+    
+    trainingLabels_ = new double[numberOfTrainingSamples_][numberOfLabels];
+    byte[] labelBuffer = new byte[1];
+    
+    for(int i = 0;i < numberOfTrainingSamples_;i++){
+      fileInputStream.read(labelBuffer);
+      
+      for(int j = 0;j < numberOfLabels;j++){
+        trainingLabels_[i][j] = 0;
+      }
+      
+      trainingLabels_[i][(int)(labelBuffer[0] & 0xFF)] = 1;
+    }
+    fileInputStream.close();
+    
+    // Load the testing set. ==================================================
+    fileInputStream = new FileInputStream(
+                        "data/testing_data/MNIST_data/t10k-images.idx3-ubyte");
+    fileInputStream.skip(16);
+    
+    testingSet_ = new double[numberOfTestingSamples_][sampleLength];
+    
+    for(int i = 0;i < numberOfTestingSamples_;i++){
+      fileInputStream.read(sampleBuffer);
+      
+      for(int j = 0;j < sampleLength;j++){
+        testingSet_[i][j] = ((double)(sampleBuffer[j] & 0xFF)) / 127.5 - 1;
+      }
+    }
+    fileInputStream.close();
+    
+    // Load the testing labels. ===============================================
+    fileInputStream = new FileInputStream(
+                        "data/testing_data/MNIST_data/t10k-labels.idx1-ubyte");
+    fileInputStream.skip(8);
+    
+    testingLabels_ = new int[numberOfTestingSamples_];
+    
+    for(int i = 0;i < numberOfTestingSamples_;i++){
+      fileInputStream.read(labelBuffer);
+      testingLabels_[i] = (int)(labelBuffer[0] & 0xFF);
+    }
+    fileInputStream.close();
+  }
+  
+  public void train() throws FileNotFoundException, IOException{
+    int numberOfLabels = sizesOfLayers_[sizesOfLayers_.length - 1];
+    
+    for(int epoch = 0;epoch < numberOfEpochs_;epoch++){
+      System.out.println("Epoch: " + epoch);
+      
+      // Train the neural network. ============================================
+      for(int batch = 0;batch < numberOfTrainingSamples_ / batchSize_;batch++){
+        neuralNetwork_.train(
+                           Arrays.copyOfRange(trainingSet_, batch * batchSize_,
+                                                     numberOfTrainingSamples_),
+                           Arrays.copyOfRange(trainingLabels_,
+                                 batch * batchSize_, numberOfTrainingSamples_),
+                           batchSize_, 1, gamma_);
+      }
+      
+      // Test the neural network. =============================================
+      int correctAnswerCounter = 0;
+      double[] output = new double[numberOfLabels];
+      for(int i = 0;i < numberOfTestingSamples_;i++){
+        output = neuralNetwork_.feedForward(testingSet_[i]);
+        
+        double max = output[0];
+        int index = 0;
+        for(int j = 0;j < output.length;j++){
+          if(output[j] > max){
+            max = output[j];
+            index = j;
+          }
+        }
+      
+        if(index == testingLabels_[i]){
+          correctAnswerCounter++;
+        }
+      }
+      System.out.println(correctAnswerCounter + " correct answers!");
+      
+    }
+    neuralNetwork_.saveNetwork(
+        "data/trained_networks/network_tr" + numberOfTrainingSamples_ + "_e" +
+         numberOfEpochs_ + "_b" + batchSize_ + "_g" + gamma_);
+  }
+
+  private int numberOfTrainingSamples_;
+  private int numberOfTestingSamples_;
+};
