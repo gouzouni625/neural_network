5cdf3c96cd85c846ac53982e85f030ba9b203ee2
Georgios Ouzounis <geoconstructor625@gmail.com>
2015-06-07 08:40:44 +0300

    main.trainers.SimpleTrainer uses DataSet class to load and save data and also saves the neural network each time a better accuracy is found.

--- src/main/trainers/SimpleTrainer.java
+++ src/main/trainers/SimpleTrainer.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.util.Arrays;
 
 import main.distorters.Distorter;
+import main.utilities.DataSet;
 
 import org.opencv.core.Core;
 
@@ -17,96 +18,52 @@ public class SimpleTrainer extends Trainer{
                                             Distorter distorter){
     super(sizesOfLayers, trainingSetPath, trainingLabelsPath,
           testingSetPath, testingLabelsPath, distorter);
-    
+
     System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-  }
-  
-  @Override
-  public void load() throws Exception{
-    numberOfLabels_ = sizesOfLayers_[sizesOfLayers_.length - 1];
-    
-    FileInputStream fileInputStream = null;
-    DataInputStream dataInputStream = null;
-    
-    // Initialize training set and testing labels.
-    trainingSet_ = new double[numberOfTrainingSamples_][sampleLength_];
-    trainingLabels_ = new double[numberOfTrainingSamples_][numberOfLabels_];
-    for(int i = 0;i < numberOfTrainingSamples_;i++){
-      for(int j = 0;j < numberOfLabels_;j++){
-        trainingLabels_[i][j] = 0;
-      }
-    }
-    
-    // Initialize testing set and testing labels.
-    testingSet_ = new double[numberOfTestingSamples_][sampleLength_];
-    testingLabels_ = new int[numberOfTestingSamples_];
-    
-    // Load the training set data. =============================================
-    fileInputStream = new FileInputStream(trainingSetPath_);
-    dataInputStream = new DataInputStream(fileInputStream);
-    
-    // Just read the first int to discard it. It will not be used. Instead,
-    // numberOfTrainingSamples_ will be used to give the ability to use less
-    // than total number of samples. It numberOfSamples_ > numberOfSamples, then
-    // an exception will be thrown.
-    int numberOfSamples = dataInputStream.readInt();
-    for(int i = 0;i < numberOfTrainingSamples_;i++){
-      for(int j = 0;j < sampleLength_;j++){
-        trainingSet_[i][j] = ((double)dataInputStream.readInt());
-      }
-    }
-    dataInputStream.close();
-  
-    // Load the training labels data. ==========================================
-    fileInputStream = new FileInputStream(trainingLabelsPath_);
-    dataInputStream = new DataInputStream(fileInputStream);
-  
-    numberOfSamples = dataInputStream.readInt();
-    for(int i = 0;i < numberOfTrainingSamples_;i++){
-      trainingLabels_[i][dataInputStream.readInt()] = 1;
-    }
-    dataInputStream.close();
-  
-    // Load the testing set data. ==============================================
-    fileInputStream = new FileInputStream(testingSetPath_);
-    dataInputStream = new DataInputStream(fileInputStream);
-    
-    // Just read the first int to discard it. It will not be used. Instead,
-    // numberOfTrainingSamples_ will be used to give the ability to use less
-    // than total number of samples. It numberOfSamples_ > numberOfSamples, then
-    // an exception will be thrown.
-    numberOfSamples = dataInputStream.readInt();
-    for(int i = 0;i < numberOfTestingSamples_;i++){
-      for(int j = 0;j < sampleLength_;j++){
-        testingSet_[i][j] = ((double)dataInputStream.readInt());
-      }
-    }
-    dataInputStream.close();
-    
-    // Load the testing labels data. ===========================================
-    fileInputStream = new FileInputStream(testingLabelsPath_);
-    dataInputStream = new DataInputStream(fileInputStream);
-    
-    numberOfSamples = dataInputStream.readInt();
-    for(int i = 0;i < numberOfTestingSamples_;i++){
-      testingLabels_[i] = dataInputStream.readInt();
-    }
-    fileInputStream.close();
-    
-    this.prepareData();
   }
 
   @Override
-  public void train() throws Exception{    
+  public void load() throws Exception{
+    DataSet trainingSet = DataSet.loadIDXFormat(trainingSetPath_, trainingLabelsPath_);
+    DataSet testingSet = DataSet.loadIDXFormat(testingSetPath_, testingLabelsPath_);
+
+    trainingSet_ = new double[trainingSet.size()][sampleLength_];
+    trainingLabels_ = new double[trainingSet.size()][numberOfLabels_];
+    for(int i = 0;i < trainingSet.size();i++){
+      for(int j = 0;j < sampleLength_;j++){
+        trainingSet_[i][j] = ((double)(trainingSet.get(i).data_[j] & 0xFF)) / 127.5 - 1;
+      }
+
+      for(int j = 0;j < numberOfLabels_;j++){
+        trainingLabels_[i][j] = 0;
+      }
+
+      trainingLabels_[i][trainingSet.get(i).label_ & 0xFF] = 1;
+    }
+
+    testingSet_ = new double[testingSet.size()][sampleLength_];
+    testingLabels_ = new int[testingSet.size()];
+    for(int i = 0;i < testingSet.size();i++){
+      for(int j = 0;j < sampleLength_;j++){
+        testingSet_[i][j] = ((double)(testingSet.get(i).data_[j] & 0xFF)) / 127.5 - 1;
+      }
+
+      testingLabels_[i] = testingSet.get(i).label_ & 0xFF;
+    }
+
+  }
+
+  @Override
+  public void train() throws Exception{
     double[][] trainingSetBuffer = new double[numberOfTrainingSamples_]
                                              [sampleLength_];
-    
+
     double bestAccuracy = 0;
     for(int epoch = 0;epoch < numberOfEpochs_;epoch++){
       if(!quiet_){
         System.out.println("Epoch: " + epoch);
       }
-      
+
       // Rotate, scale and translate each training sample, to virtually
       // increase the training set's size. The distortions are applied on the
       // initial training set and not on the already distorted.
@@ -130,13 +87,13 @@ public class SimpleTrainer extends Trainer{
             }
           }
         }
-        
+
         trainingSet_ = distorter_.distort(trainingSet_);
         if(!quiet_){
           System.out.println("Distortion is done!");
         }
       }
-      
+
       // Actually train the neural network.
       for(int batch = 0;batch < numberOfTrainingSamples_ / batchSize_;batch++){
         neuralNetwork_.train(Arrays.copyOfRange(trainingSet_,
@@ -145,16 +102,16 @@ public class SimpleTrainer extends Trainer{
                                  batch * batchSize_, numberOfTrainingSamples_),
                             batchSize_, 1, gamma_);
       }
-      
+
       // Test the result on each epoch.
       int correctAnswerCounter = 0;
-      double[] output = new double[10];
+      double[] output = new double[numberOfLabels_];
       for(int i = 0;i < numberOfTestingSamples_;i++){
         output = neuralNetwork_.feedForward(testingSet_[i]);
 
         double max = output[0];
         int index = 0;
-        for(int j = 0;j < 10;j++){
+        for(int j = 0;j < numberOfLabels_;j++){
           if(output[j] > max){
             max = output[j];
             index = j;
@@ -165,13 +122,16 @@ public class SimpleTrainer extends Trainer{
           correctAnswerCounter++;
         }
       }
-      
+
       double accuracy = (double)correctAnswerCounter / numberOfTestingSamples_;
       accuracy *= 100;
       if(accuracy > bestAccuracy){
         bestAccuracy = accuracy;
+
+        System.out.println("Found best accuracy, saving the neural network!");
+        neuralNetwork_.saveNetwork(neuralNetworkSavePath_);
       }
-      
+
       if(!quiet_){
         System.out.println(correctAnswerCounter + "/" +
                                  numberOfTestingSamples_ + " correct answers!");
@@ -179,7 +139,8 @@ public class SimpleTrainer extends Trainer{
         System.out.println(accuracy + "%");
         System.out.println("Best so far: " + bestAccuracy + "%");
       }
-      
+
     }
-  } 
+  }
+
 }
